bd1c83f49774ea3e5905d47c14c094a2ad487a31
siebe.albers <siebe.albers@telepathy.ai>
2022-03-17 09:20:13 +0100

    removing special symbols that arent encoded adequately

--- e2e_normalization.sh
+++ e2e_normalization.sh
@@ -6,21 +6,33 @@ LANGUAGE=en
 # input=zsa_input.txt
 # input=examples/en/zsa_z_input_sentences.txt
 # input=examples/en/zsa_z_input.txt
-input=ATN_input.txt
+input0=ATN_input.txt
 output=.output
 
 # ASR_CFG=text.asr.txt
 TTS_CFG=tts_siebe.cfg
 # NONE_CFG=siebe_none.cfg
 
+
 echo -n "Starting process at: "; date; printf '\n'
 echo "on input file:"; echo $input; printf '\n'
 
+#=================================================
+#  salb deleting characters that are wrongly encoded
+#================================================
+# salb don't want to edit the original input file, therefore:
+cp $input0 input.txt
+input=input.txt
+# removing; •, 
+sed -i -e "s/•/-/g" -e "s/fiat//g" $input
+
+
+
 # General normalization
-echo "Tokenization..."
+echo "1. Tokenization..."
 perl $ROOT/bin/$LANGUAGE/basic-tokenizer.pl $input > $output.1tok
 
-echo "Generic normalization start..."
+echo "2. Generic normalization start..."
 perl $ROOT/bin/$LANGUAGE/start-generic-normalisation.pl $output.1tok > $output.2start
 #===========================================================
 # "  salb replacements               "
@@ -44,10 +56,10 @@ echo
 #===========================================================
 #                
 #==========================================================
-echo "Currency conversion..."; echo "!!!!!!SOURCING FROM tl_lm_resources/normalizers/irisa_normalizer/convert_currencies.pl "
+echo "3. Currency conversion..."; echo "!!!!!!SOURCING FROM tl_lm_resources/normalizers/irisa_normalizer/convert_currencies.pl "
 perl $HOME/dev/tl_lm_resources/normalizers/irisa_normalizer/convert_currencies.pl $output.2start > $output.3currency_fix.txt
 
-echo "Generic normalization end..."
+echo "4. Generic normalization end..."
 perl $ROOT/bin/$LANGUAGE/end-generic-normalisation.pl $output.3currency_fix.txt > $output.4general_norm.txt
 
 
@@ -56,7 +68,7 @@ perl $ROOT/bin/$LANGUAGE/end-generic-normalisation.pl $output.3currency_fix.txt 
 # echo " "
 # perl $ROOT/bin/$LANGUAGE/specific-normalisation.pl $ROOT/cfg/$ASR_CFG $output.4general_norm.txt > $output.5asr.txt
 
-echo "TTS specific normalization..."
+echo "5. TTS specific normalization..."
 perl $ROOT/bin/$LANGUAGE/specific-normalisation.pl $ROOT/cfg/$TTS_CFG $output.4general_norm.txt > $output.5tts.txt
 
 # # sa try to source from earlier output:

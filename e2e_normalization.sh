7ef707161f337a25be8be1ae24c9b9388b76de1f
siebe.albers <siebe.albers@telepathy.ai>
2022-03-24 07:44:18 +0100

    IMPR[TLZD-284] replacing sed for perl to make it work on mac as well + reformat obsolete comments

--- e2e_normalization.sh
+++ e2e_normalization.sh
@@ -6,19 +6,21 @@ LANGUAGE=en
 # input=zsa_input.txt
 # input=examples/en/zsa_z_input_sentences.txt
 # input=examples/en/zsa_z_input.txt
+
 ls -l *.txt # show the user options of file that can be inputted
 read -p 'insert the name of the file (not the goldStandard of course) that you want to normalize by the ATN tool: ' input0 # ask for user input
+input0='ATN.txt'
 
 echo "The current directory is : $current_dir"
 
-# for storing the output file under a convenient way:
+# for automatically naming the output file:
 output_file_name=$(echo $input0) # store the name of $input0 as a string 
-# replace the patterns in the inptu file name that wanted for use in the$output file:
-output_file_name=$(sed 's/raw_//' <<< $output_file_name)
-output_file_name=$(sed 's/.txt//' <<< $output_file_name)
+# replace the patterns in the input file name that wanted for use in the$output file:
+# output_file_name=$(sed 's/raw_//' <<< $output_file_name)
+output_file_name=$(echo $output_file_name | perl -pe 's/.txt//')
+echo $output_file_name
 # and use that for naming the$output file names:
 output=$output_file_name
-
 
 
 # ASR_CFG=text.asr.txt
@@ -31,29 +33,27 @@ echo "on input file:"; echo $input0; printf '\n'
 #=================================================
 #  salb deleting characters that are wrongly encoded
 #================================================
-# salb don't want to sed the original input file, therefore:
+# salb don't want to manipulate the original input file, therefore
 cp $input0 .input.txt
 input=.input.txt
 
 # removing special symbols
-sed -i -e "s/•/-/g" -e "s/SIEBE//g" $input #  example of chaining sed commands
-sed -i "s/”/'/g" $input
-sed -i "s/“/'/g" $input
-sed -i "s/—/--/g" $input
-sed -i "s/–/--/g" $input
-sed -i "s/‘/'/g" $input
-sed -i "s/’/'/g" $input
-sed -i "s/…/\.\.\./g" $input
-sed -i "s/®//g" $input
-sed -i "s/™//g" $input
+perl -0777 -pi.orig -e "s/•/-/g" $input
+perl -0777 -pi.orig -e "s/”/'/g" $input
+perl -0777 -pi.orig -e "s/“/'/g" $input
+perl -0777 -pi.orig -e "s/—/--/g" $input
+perl -0777 -pi.orig -e "s/–/--/g" $input
+perl -0777 -pi.orig -e "s/‘/'/g" $input
+perl -0777 -pi.orig -e "s/’/'/g" $input
+perl -0777 -pi.orig -e "s/…/\.\.\./g" $input
+perl -0777 -pi.orig -e "s/®//g" $input
+perl -0777 -pi.orig -e "s/™//g" $input
 
 # General normalization
 echo "1. Tokenization..."
 perl $ROOT/bin/$LANGUAGE/basic-tokenizer.pl $input > .$output.1tok
 
-# getting rid of a broken line e.g.:
-# 20 percent
- # to accommodate
+# fixing line breaks done by IRISA, causing line length difference between ATN ^ MTN/raw
 perl -0777 -pi.orig -e 's/([a-z])\n\s([a-z])/\1 \2/' .$output.1tok
 # the 0777 flag: https://stackoverflow.com/questions/71556049/regex-does-not-match-in-perl-while-it-does-in-other-programs
 # it processes all as one string, not one line per
@@ -63,72 +63,39 @@ perl -pi.orig -e 's/(\w)(\s-\s)(\w)/\1: \3/g' .$output.1tok
 perl -pi.orig -e 's/(\b\s\(\.[0-9]+\)\s\b)//g' .$output.1tok
 
 
-
 echo "2. Generic normalization start..."
 perl $ROOT/bin/$LANGUAGE/start-generic-normalisation.pl .$output.1tok > .$output.2start
 #===========================================================
 # "  salb replacements               "
 #==========================================================
-echo 'making salb replacements'
-# slitting out e.g. 'E5--> E 5'
-sed -i -E "s/([a-zA-Z])([0-9])/\1  \2/" .$output.2start
+# splitting out e.g. 'E5--> E 5'
+perl -0777 -pi.orig -e "s/([a-zA-Z])([0-9])/\1  \2/" .$output.2start
 # splitting out e.g. '100k --> 100 k'
-sed -i -E 's/([0-9]+)([a-zA-Z])/\1 \2/g' .$output.2start
+perl -0777 -pi.orig -e 's/([0-9]+)([a-zA-Z])/\1 \2/g' .$output.2start
 # echo "salb replacing percentages"
-sed -i -e 's/%/ percent/' .$output.2start
+perl -0777 -pi.orig -e 's/%/ percent/' .$output.2start
 
 #===========================================================
 #                
 #==========================================================
 echo "3. Currency conversion..."
-# echo "!!!!!!SOURCING FROM tl_lm_resources/normalizers/irisa_normalizer/convert_currencies.pl "
-perl $$ROOT/convert_currencies.pl .$output.2start > .$output.3currency_fix.txt
+perl $ROOT/convert_currencies.pl .$output.2start > .$output.3currency_fix.txt
 
 echo "4. Generic normalization end..."
 perl $ROOT/bin/$LANGUAGE/end-generic-normalisation.pl .$output.3currency_fix.txt > .$output.4general_norm.txt
 
-
-# echo "ASR specific normalization..., with the cfg file:"
-# echo $ASR_CFG
-# echo " "
-# perl $ROOT/bin/$LANGUAGE/specific-normalisation.pl $ROOT/cfg/$ASR_CFG .$output.4general_norm.txt > .$output.5asr.txt
-
 echo "5. TTS specific normalization..."
 perl $ROOT/bin/$LANGUAGE/specific-normalisation.pl $ROOT/cfg/$TTS_CFG .$output.4general_norm.txt > $output.5tts.txt
 
-# # sa try to source from earlier$output:
-# echo "TTS specific normalization..."
-# perl $ROOT/bin/$LANGUAGE/specific-normalisation.pl $ROOT/cfg/$TTS_CFG $output.start > $output.tts.txt
-
-
-# Remove some punctuation:
-# sed -i "s/\./ /g" $output.asr.txt
-# sed -i "s/,/ /g" $output.asr.txt
-# sed -i 's/ \+ / /g' $output.asr.txt
-
-# sed -i "s/\./ /g" $output.5tts.txt
-# sed -i "s/,/ /g" $output.5tts.txt
-# sed -i 's/ \+ / /g' $output.5tts.txt
-
-# salb specific
-sed -i 's/^\(.\)/\U\1/' $output.5tts.txt # case first letter of a sentence
-# TODO capitilize every word after strong punctuation
-
 
 # removing space between punctution
-sed -i 's/ \([.?,\/#!$%\^&\*;:{}=\-_`~()]\)/\1/g' $output.5tts.txt
+perl -0777 -pi.orig -e 's/(\s)([\.\!\,\?\;])/$2/g' $output.5tts.txt
 
-# sed -i 's/\([A-Z]\)\([0-9]\)/\1 \2/g' $output
 
 # Remove empty lines in ASR and TTS:
-echo
-echo "Removing empty lines..."
-# sed -i '/^\s*$/d' $output.asr.txt
-sed -i '/^\s*$/d' $output.5tts.txt
+perl -0777 -pi.orig -e s'/^\s*\n//mg;' $output.5tts.txt
 
 
-# salb removing obsolete files:
-rm .input.txt
 
 
 #===========================================================
@@ -141,15 +108,14 @@ rm .input.txt
 perl -0777 -pi.orig -e 's/([\.\?\!]\s*)([a-z])/$1\U$2/g' $output.5tts.txt
 
 
-# Finished
 echo
 echo -n "Done. Finished at: "; date; printf '\n the file is saved under:'; printf $output_file_name; printf '_5tts.txt \n'
-# echo $(date)
 
 
-# salb removing redundant file names in dir
+# salb removing obsolete files:
+rm .input.txt
 rm .$output_file_name*
 rm *\.orig
 echo
-echo 'end of the ATN normalization program'
+echo 'The end of the ATN normalization program'
 echo
